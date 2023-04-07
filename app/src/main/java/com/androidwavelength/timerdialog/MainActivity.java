36e202b06c155780130efee893aaba5960c78186
maithileekarnekar <maithileekarnekar17@gmail.com>
2023-04-07 16:33:54 +0530

    TimerDialog

---
+++ app/src/main/java/com/androidwavelength/timerdialog/MainActivity.java
@@ -0,0 +1,32 @@
+package com.androidwavelength.timerdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected TimerDialog timerDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showTimerDialog();
+    }
+
+    protected void showTimerDialog() {
+        timerDialog = new TimerDialog(this);
+        timerDialog.setOnTimerExpired(new MyOnTimerExpiredListener());
+        timerDialog.setTimer(1,1,9);
+        timerDialog.show();
+    }
+
+    private class MyOnTimerExpiredListener implements TimerDialog.OnTimerExpired {
+        @Override
+        public void onTimerExpired() {
+
+        }
+    }
+}
